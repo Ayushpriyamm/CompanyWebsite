10144eaf367c3a7b686a4be7c2a46af30a765917
Ayushpriyamm <ayushpriyam24@gmail.com>
2025-05-25 13:17:47 +0530

    commit check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-red-500 text-center font-bold'>Hello</h1>
+      <h1 className='text-red-500 text-center font-bold'>Pritech Solutions</h1>
     </>
   )
 }
